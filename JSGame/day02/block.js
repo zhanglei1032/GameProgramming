0e0a5eee52d911480d830cfefeb7c89be097698a
zhanglei1032 <zhanglei1032@gmail.com>
2018-04-18 08:51:28 +0800

    添加关卡

--- JSGame/day02/block.js
+++ JSGame/day02/block.js
@@ -1,9 +1,11 @@
-var Block = function () {
+var Block = function (position) {
+    //position是[0，0]格式的
+    var p = position
     var image = imageFromPath('block.png')
     var o = {
         image: image,
-        x: 100,
-        y: 50,
+        x: p[0],
+        y: p[1],
         w: 40,
         h: 20,
         alive: true,
